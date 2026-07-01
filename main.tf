3add009d2030d2a49066f47c6cfda53e4eb85cbb
Leon Pereira <lee_pereira@msn.com>
2026-07-01 15:28:55 -0700

    Initial GKE Terraform config

---
+++ main.tf
@@ -0,0 +1,97 @@
+resource "google_compute_network" "vpc_network" {
+  name                    = "custom-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "custom_subnet" {
+  name          = "custom-subnet"
+  ip_cidr_range = "10.10.0.0/16"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_firewall" "allow_internal" {
+  name    = "allow-internal"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "all"
+  }
+
+  source_ranges = ["10.10.0.0/16"]
+}
+
+resource "google_compute_firewall" "allow_external" {
+  name    = "external-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "3389"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+
+}
+
+resource "google_compute_firewall" "allow_gke" {
+  name    = "gke-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443", "10250", "15017"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+#GKE Cluster
+resource "google_container_cluster" "primary" {
+  project            = var.project_id
+  name               = "gke-cluster"
+  location           = var.zone # region for control plane
+  initial_node_count = 1
+
+  network                  = google_compute_network.vpc_network.id
+  subnetwork               = google_compute_subnetwork.custom_subnet.id
+  min_master_version       = var.K8s_version
+  deletion_protection      = false
+  remove_default_node_pool = true
+
+
+}
+
+
+resource "google_container_node_pool" "primary_nodes" {
+  project        = var.project_id
+  name           = "primary-node-pool"
+  location       = var.zone # region : eg : us-central1
+  cluster        = google_container_cluster.primary.name
+  version        = var.K8s_version
+  node_locations = [var.zone] # use to pick specific zone within region for node pool
+  node_count     = var.node_count
+
+  node_config {
+    image_type   = "UBUNTU_CONTAINERD"
+    disk_size_gb = 20
+    disk_type    = "pd-standard"
+    machine_type = "e2-medium"
+
+  }
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
+
+  management {
+    auto_upgrade = true
+    auto_repair  = true
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,50 @@
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+  default     = "playground-s-11-838df330"
+
+}
+
+variable "region" {
+  description = "The region in which to provision resources."
+  type        = string
+  default     = "us-central1"
+}
+
+
+variable "zone" {
+  description = "The zone in which to provision resources."
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "credentials_file" {
+  description = "The path to the service account key file in JSON format."
+  type        = string
+  default     = "keys.json"
+}
+
+variable "backend_bucket" {
+  description = "The name of the GCS bucket to use for Terraform state."
+  type        = string
+  default     = "gke-terraform-bucket01"
+}
+
+variable "backend_prefix" {
+  description = "The prefix to use for the Terraform state file in the GCS bucket."
+  type        = string
+  default     = "terraform/state"
+}
+
+variable "K8s_version" {
+  description = "The version of the GKE master to use."
+  type        = string
+  default     = "1.26.9-gke.1000"
+}
+
+
+variable "node_count" {
+  description = "The number of nodes to create in the node pool."
+  type        = number
+  default     = 1
+}
